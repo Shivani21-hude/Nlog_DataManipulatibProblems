b123dc9248a6ae8e0d029ef5e326cba1d12e7f85
Shivani21-hude <shivani21hude@gmail.com>
2023-03-26 10:49:11 +0530

    [Shivani]-Using NLog

---
+++ NlogSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using NlogSample;
+using System;
+namespace NLogSample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Adding.Sum(2, 3);
+            Console.WriteLine();
+            Console.WriteLine("This is a Logger Demo");
+            
+            Console.ReadKey();
+        }
+    }
+}
